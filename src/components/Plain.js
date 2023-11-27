538287f043c0e7bbe029a1ed28c49570dc33021c
Jordan Porter <silentsummitsoftware@gmail.com>
2023-11-27 12:23:08 -0700

    fix sizing issues

--- src/components/Plain.js
+++ src/components/Plain.js
@@ -3,7 +3,7 @@
 export default function Plain(props){
     return(
         <div id={props.hash || ''} className={`flex center padded center-horiz ${props.classes || ''}` }>
-            <div className='regular logo-blue-color poppins text-center'>
+            <div className='regular logo-blue-color poppins text-center contain-width padded'>
                 {props.title ? <div className={'bold-mid ' + props.titleClasses || ''}>{props.title}</div> : ''}
                 <div className='bold-smaller logo-blue-color poppins text-center'>{props.secondaryText}</div>
                 {props.children}
