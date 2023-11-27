c55be7202b2371edd4379f5812cdc6f046bc3a96
Jordan Porter <silentsummitsoftware@gmail.com>
2023-11-26 22:22:11 -0700

    add text blurb

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ function Main() {
       <Frosting componentSize='full'>
         CUSTOM BUTTERCREAM CAKES
       </Frosting>
-      <Plain title="WHAT WE DO" classes='logo-alt-pink-bg half margin' secondaryText="this is some gibberish garbage. this is some gibberish garbage. this is some gibberish garbage. this is some gibberish garbage. ">
+      <Plain title="WHAT WE DO" classes='logo-alt-pink-bg half margin' secondaryText="We are a gluten-free custom cake shop run by a baker with Celiac disease who loves to make custom lambeth style buttercream cakes to order. We use high-quality gluten-free ingredients and buttercream frosting to make our cakes moist, delicious, and beautiful to look at. We also offer a variety of flavors and fillings to suit your taste buds and strive to create our cakes to order to match your unique vision. But that’s not all. We also make miniature “cutiecakes”, which are a 4 inch version of our lambeth buttercream cake. These are ideal for smaller servings, gifts, or party favors. They are cute, adorable, and just as delicious as our regular lambeth cakes. If you are looking for a unique, tasty way to celebrate your next event with gluten-free options, contact us today and let us know your requirements. We will work with you to design and create a custom lambeth cake or cutiecakes that will make your day extra special. Thank you for choosing Frosted By Nes!">
       </Plain>
       <ImageContainer classes='full' children={[
         {src: 'lamberth_transparent.png', text: 'CAKES', classes:'contain-height', link:'/pricing#6-inch-rounds'},
